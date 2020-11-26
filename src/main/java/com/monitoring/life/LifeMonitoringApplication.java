e71ef9a50bfa432b691955231f39c740bcda4bc9
4m1t0 <fum1t0.eng@gmail.com>
2020-11-26 22:06:34 +0900

    rename package

--- src/main/java/com/monitoring/life/LifeMonitoringApplication.java
+++ src/main/java/com/monitoring/life/LifeMonitoringApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.monitoring.life;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LifeMonitoringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LifeMonitoringApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LifeMonitoringApplication.class, args);
+  }
 }
